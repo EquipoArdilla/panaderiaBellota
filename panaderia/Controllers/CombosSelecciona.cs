6215b540bf8b74e76f6ed6274ada4c80b1039b24
Palmatori <33674126+palmatori@users.noreply.github.com>
2018-06-09 11:47:32 -0400

    Actualiza Receta y Detalle Receta
    Para el menún se debe usar Recetas

---
+++ panaderia/Controllers/CombosSelecciona.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using panaderia.Models;
+
+    public class CombosSelecciona
+    {
+        public List<producto> producto { get; set; }
+        public List<medida> medida { get; set; }
+        public List<receta> receta { get; set; }
+        public List<detalle_receta> detalle_receta { get; set; }
+        public int? idSeleccionado { get; internal set; }
+    }
