dc6672301dbd200e0046432665693107e536e133
Palmatori <33674126+palmatori@users.noreply.github.com>
2018-06-12 22:29:26 -0400

    Actulización controler y view

---
+++ panaderia/Controllers/RecetasController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using panaderia.Models;
+
+namespace panaderia.Controllers
+{
+    public class RecetasController : Controller
+    {
+        private PanaderiaEntities db = new PanaderiaEntities();
+
+        // GET: Recetas
+        public ActionResult Index()
+        {
+            return View(db.receta.ToList());
+        }
+
+        // GET: Recetas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            receta receta = db.receta.Find(id);
+            if (receta == null)
+            {
+                return HttpNotFound();
+            }
+            return View(receta);
+        }
+
+        // GET: Recetas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Recetas/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,nombre")] receta receta)
+        {
+            if (ModelState.IsValid)
+            {
+                db.receta.Add(receta);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(receta);
+        }
+
+        // GET: Recetas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            receta receta = db.receta.Find(id);
+            if (receta == null)
+            {
+                return HttpNotFound();
+            }
+            return View(receta);
+        }
+
+        // POST: Recetas/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,nombre")] receta receta)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(receta).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(receta);
+        }
+
+        // GET: Recetas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            receta receta = db.receta.Find(id);
+            if (receta == null)
+            {
+                return HttpNotFound();
+            }
+            return View(receta);
+        }
+
+        // POST: Recetas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            receta receta = db.receta.Find(id);
+            db.receta.Remove(receta);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
