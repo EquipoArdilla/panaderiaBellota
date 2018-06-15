8b999478309234a960cad9e67e0e97c8a4270fc9
folken <folken1492@gmail.com>
2018-06-15 19:27:30 -0400

    prueba unitaria
    prueba unitaria a prodcuto

---
+++ panaderia/Test/productoesControllertest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using panaderia;
+using panaderia.Controllers;
+
+namespace panaderia.Tests.Controllers
+{
+    [TestClass]
+    public class ProductoesControllertest
+    {
+        [TestMethod]
+        public void IndexProductoNoNulo()
+        {
+            // Arrange
+            ProductoesController controller = new ProductoesController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            //Assert.AreEqual(3,result.ViewBag.id );
+        }
+    }
+}
