b8bca4b3e1ab21887f7e6a730ce5c25b04297187
sebastianoyarzun <soportelaunion@gmail.com>
2018-06-16 09:30:14 -0300

    Feature-ReporteJornada
    Feature-ReporteJornada

---
+++ panaderia/Controllers/ReporteProduccionController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using panaderia.Models;
+
+namespace panaderia.Controllers
+{
+    public class ReporteProduccionController : Controller
+    {
+        private PanaderiaEntities db = new PanaderiaEntities();
+
+        // GET: ReporteProduccion
+        public ActionResult Index()
+        {
+            var produccion = db.produccion.Include(p => p.receta);
+            return View(produccion.ToList());
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
