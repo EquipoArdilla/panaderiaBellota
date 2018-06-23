8e22c20319046befc9ab65ded34c63d8c96205fb
silvanaparedesa <38157991+silvanaparedesa@users.noreply.github.com>
2018-06-23 10:11:33 -0400

    Modifica Pruebas unitarias RolUsuario
    Modifica Pruebas unitarias RolUsuario

---
+++ panaderia/test/RolUsuarioControllerTest.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using panaderia;
+using panaderia.Controllers;
+
+namespace panaderia.Tests.Controllers
+{
+    [TestClass]
+    public class RolUsuarioControllerTest
+    {
+        int Id = 3;
+        //string nombre = "Pruebas ";
+
+        [TestMethod]
+        public void Index()
+        {
+
+            rolusuariosController controller = new rolusuariosController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+
+            //Assert.AreEqual("Details", result.ViewName);
+
+        }
+
+
+        [TestMethod]
+        public void Create()
+        {
+
+            lineasController controller = new lineasController();
+
+            // Act
+            ViewResult result = controller.Create() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+
+            //Assert.AreEqual("Details", result.ViewName);
+
+        }
+
+
+        [TestMethod]
+        public void Edit()
+        {
+
+            lineasController controller = new lineasController();
+
+            // Act
+            ViewResult result = controller.Edit(Id) as ViewResult;
+
+            // Assert
+
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void Details()
+        {
+
+            lineasController controller = new lineasController();
+
+            // Act
+            ViewResult result = controller.Details(Id) as ViewResult;
+
+            // Assert
+
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void Delete()
+        {
+
+            lineasController controller = new lineasController();
+
+            // Act
+            ViewResult result = controller.Delete(Id) as ViewResult;
+
+            // Assert
+
+            Assert.IsNotNull(result);
+        }
+
+
+    }
+}
